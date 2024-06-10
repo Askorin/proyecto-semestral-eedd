779e0fcc7cae856945bfae5abfd521c830930705
Bastián Ceballos Z <bceballos2022@udec.cl>
2024-06-09 22:19:25 -0400

    Feat:  decodificación Huffman

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@ int main() {
     string message = "alabar_a_la_alabarda";
     auto code = encode(message);
     auto coded_message = get<0>(code);
-    //auto tree = get<1>(code);
+    auto tree = get<1>(code);
     std::cout << coded_message << std::endl;
     std::cout << "Longitud del mensaje: " << coded_message.size() << std::endl;
+    auto decoded_message = decode(coded_message, *tree);
+    std::cout << decoded_message << std::endl;
     return 0;
 }
