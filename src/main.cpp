4049a9d68d0df2c3aa450a5fc1833f0907a04ff1
Antonio Skorin <antonio.skorin@gmail.com>
2024-06-16 12:44:41 -0400

    Feat/Perf: Huffman mejorado, más rápido.

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,14 @@
 
 using namespace std;
 int main() {
-    string encode_file_name_input = "../data/sample.txt";
-    string encode_file_name_output = "../data/sample_compressed";
-    string decode_file_name_output = "../data/sample_decompressed.txt";
+    //string encode_file_name_input = "../data/sample.txt";
+    //string encode_file_name_output = "../data/sample_compressed";
+    //string decode_file_name_output = "../data/sample_decompressed.txt";
+
+    string encode_file_name_input = "../data/english.txt";
+    string encode_file_name_output = "../data/compressed";
+    string decode_file_name_output = "../data/decompressed.txt";
+
 
     encode_file(encode_file_name_input, encode_file_name_output);
     decode_file(encode_file_name_output, decode_file_name_output);
