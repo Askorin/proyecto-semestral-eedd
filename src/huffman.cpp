de51e7cbec7301b320d426e65b237319d3317202
Antonio Skorin <antonio.skorin@gmail.com>
2024-06-16 19:54:59 -0400

    Refactor: Ahora se usan arreglos, no mapas.

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -12,8 +12,9 @@ bool CompareHuffmanNodes::operator()(Node* node_l, Node* node_r) {
     return node_l->freq > node_r->freq;
 }
 
-bool CompareCodes::operator()(std::pair<char, unsigned char> symbol_l,
-                              std::pair<char, unsigned char> symbol_r) {
+bool CompareCodes::operator()(
+    std::pair<unsigned char, unsigned char> symbol_l,
+    std::pair<unsigned char, unsigned char> symbol_r) {
     /* Si la longitud del código es la misma, comparamos por orden de su código
      * ascii. */
     if (symbol_l.second == symbol_r.second)
@@ -23,7 +24,8 @@ bool CompareCodes::operator()(std::pair<char, unsigned char> symbol_l,
         return symbol_l.second > symbol_r.second;
 }
 
-Node::Node(char symbol, size_t freq, Node* left, Node* right, Node* parent)
+Node::Node(unsigned char symbol, size_t freq, Node* left, Node* right,
+           Node* parent)
     : symbol(symbol), freq(freq), left(left), right(right), parent(parent) {}
 
 Node::Node(Node* node_l, Node* node_r)
@@ -40,14 +42,14 @@ std::ostream& operator<<(std::ostream& os, const Node& node) {
     return (os << node.symbol << " : " << node.freq);
 }
 
-Code::Code(std::bitset<255> code, unsigned char length)
+Code::Code(std::bitset<MAX_CODE_SIZE> code, unsigned char length)
     : code(code), length(length) {}
 
-Code::Code() : code(std::bitset<255>()), length(0) {}
+Code::Code() : code(std::bitset<MAX_CODE_SIZE>()), length(0) {}
 
 Code Code::get_reversed() {
-    std::bitset<255> reversed;
-    std::bitset<255> to_reverse = code;
+    std::bitset<MAX_CODE_SIZE> reversed;
+    std::bitset<MAX_CODE_SIZE> to_reverse = code;
     for (size_t i = 0; i < length; ++i) {
         reversed[length - 1 - i] = to_reverse[0];
         to_reverse >>= 1;
@@ -68,42 +70,33 @@ void encode_file(std::string& file_name_input, std::string& file_name_output) {
      * en verdad lo único que importa. Una vez tengamos estos, podremos
      * construir los códigos canónicos.
      */
-    std::unordered_map<char, unsigned char> code_map;
+    std::array<unsigned char, CHAR_NUM> code_length_map{0};
 
     /*
      * Mapa que guardará la frecuencia de una longitud de código dada, utilizado
      * para Huffman canónico
      */
-    std::unordered_map<unsigned char, size_t> length_map;
+    std::array<size_t, CHAR_NUM> length_frequency_map{0};
 
     /* Longitud total del mensaje codificado */
     size_t total_len = 0;
     /* Máxima longitud de los códigos */
     size_t max_length = 0;
-    traverse_huffman_tree(root, code_map, 0, total_len, length_map, max_length);
+    traverse_huffman_tree(root, code_length_map, total_len,
+                          length_frequency_map, max_length);
 
     /* Este vector servirá para guardar los símbolos en orden el de las
      * longitudes de sus códigos */
-    std::vector<char> ordered_symbols;
+    std::vector<unsigned char> ordered_symbols;
 
     /*
      * Generamos los códigos canónicos
      */
-    std::unordered_map<char, Code> canonical_codes =
-        get_canonical_codes(code_map, ordered_symbols);
-
-    // std::cout << "Códigos Canónicos:\n";
-    // for (auto entry : canonical_codes) {
-    //     char c = entry.first;
-    //     boost::dynamic_bitset<> code = entry.second.code;
-    //     code.resize(entry.second.length);
-    //     std::cout << c << ", " << int(c) << ": " << code << std::endl;
-    // }
-
-    boost::dynamic_bitset<> coded_message(total_len);
+    std::array<Code, CHAR_NUM> canonical_codes =
+        get_canonical_codes(code_length_map, ordered_symbols);
 
     /* Codificamos */
-    std::ifstream fin(file_name_input, std::fstream::in);
+    std::ifstream fin(file_name_input, std::fstream::in  | std::fstream::binary);
 
     std::ofstream fout(file_name_output, std::ofstream::out |
                                              std::ofstream::trunc |
@@ -113,14 +106,15 @@ void encode_file(std::string& file_name_input, std::string& file_name_output) {
      * Para el primer tipo de header se guardan las longitudes de los códigos
      * para TODOS los símbolos del alfabeto.
      * */
-    size_t tamaño_header_tipo_1 = 255;
+    size_t tamaño_header_tipo_1 = CHAR_NUM;
     /*
      * Para el segundo tipo se guardan las frecuencias de cada longitud de
      * código y luego los símbolos ordenados con respecto a longitud de código.
      * Se deben guardar las frecuencias de longitudes para cada longitud menor o
      * igual que la máxima, eso incurre 1 * max_length bytes. Luego,
-     * evidentemente, para guardar los símbolos es el tamaño del vector de
-     * ellos. Además se incluye un separador de un byte netre las dos.
+     * evidentemente, para guardar los símbolos se necesita el tamaño del vector
+     * de ellos en bytes. Además se incluye un separador de un byte entre las
+     * dos.
      */
     size_t tamaño_header_tipo_2 = max_length + ordered_symbols.size() + 1;
 
@@ -129,38 +123,34 @@ void encode_file(std::string& file_name_input, std::string& file_name_output) {
     } else
         std::cout << "Usando header tipo 2\n";
 
-    save_header(fout, canonical_codes, length_map, ordered_symbols,
+    save_header(fout, canonical_codes, length_frequency_map, ordered_symbols,
                 message_len);
-    save_code(fout, fin, canonical_codes, length_map, ordered_symbols,
+    save_code(fout, fin, canonical_codes, length_frequency_map, ordered_symbols,
               message_len);
 
     fin.close();
     fout.close();
-    return;
 }
 
-std::unordered_map<char, Code>
-get_canonical_codes(std::unordered_map<char, unsigned char>& code_map,
-                    std::vector<char>& ordered_symbols) {
+std::array<Code, CHAR_NUM>
+get_canonical_codes(std::array<unsigned char, CHAR_NUM>& code_length_map,
+                    std::vector<unsigned char>& ordered_symbols) {
 
     /* Primero tenemos que ordenar los códigos, usamos una priority queue. */
-    std::priority_queue<std::pair<char, unsigned char>,
-                        std::vector<std::pair<char, unsigned char>>,
+    std::priority_queue<std::pair<unsigned char, unsigned char>,
+                        std::vector<std::pair<unsigned char, unsigned char>>,
                         CompareCodes>
         prio_queue;
 
-    for (auto entry : code_map) {
-        prio_queue.push(entry);
+    for (size_t c = 0; c < CHAR_NUM; ++c) {
+        if (code_length_map[c]) {
+            prio_queue.push(std::pair((unsigned char)c, code_length_map[c]));
+        }
     }
 
-    std::unordered_map<char, Code> canonical_codes;
+    std::array<Code, CHAR_NUM> canonical_codes{Code()};
 
-    /*
-     * Observación: En el peor caso, para los 256 carácteres ASCII, podríamos
-     * terminar con un código de 255 bits, asi que guardaremos los códigos en un
-     * bitset de ese tamaño.
-     */
-    std::bitset<255> code;
+    std::bitset<MAX_CODE_SIZE> code;
     /*
      * Al primer símbolo se le asigna un código de la misma longitud, compuesto
      * únicamente por ceros.
@@ -184,9 +174,10 @@ get_canonical_codes(std::unordered_map<char, unsigned char>& code_map,
 }
 
 void save_header(std::ofstream& fout,
-                 std::unordered_map<char, Code>& canonical_codes,
-                 std::unordered_map<unsigned char, size_t>& length_map,
-                 std::vector<char>& ordered_symbols, size_t message_len) {
+                 std::array<Code, CHAR_NUM>& canonical_codes,
+                 std::array<size_t, CHAR_NUM>& length_map,
+                 std::vector<unsigned char>& ordered_symbols,
+                 size_t message_len) {
 
     std::cout << "### Guardando header ###\n";
     /*
@@ -208,15 +199,15 @@ void save_header(std::ofstream& fout,
      */
 
     /* Longitud del mensaje */
+    std::cout << "Message len: " << message_len << '\n';
     fout.write(reinterpret_cast<char*>(&message_len), 4);
 
     /* Longitudes de códigos de cada símbolo */
-    for (size_t c = 0; c <= 255; ++c) {
+    for (size_t c = 0; c < CHAR_NUM; ++c) {
         unsigned char bit_length = 0;
-        /* Si el carácter está en el mapa su código tiene longitud no cero
-         */
-        if (canonical_codes.count(char(c))) {
-            bit_length = canonical_codes[char(c)].length;
+        /* Si el carácter tiene un código de longitud no zero */
+        if (canonical_codes[(unsigned char)c].length) {
+            bit_length = canonical_codes[(unsigned char)c].length;
         }
 
         fout.write(reinterpret_cast<char*>(&bit_length), sizeof(bit_length));
@@ -229,22 +220,25 @@ void save_header(std::ofstream& fout,
 }
 
 void save_code(std::ofstream& fout, std::ifstream& fin,
-               std::unordered_map<char, Code>& canonical_codes,
-               std::unordered_map<unsigned char, size_t>& length_map,
-               std::vector<char>& ordered_symbols, size_t message_len) {
+               std::array<Code, CHAR_NUM>& canonical_codes,
+               std::array<size_t, CHAR_NUM>& length_map,
+               std::vector<unsigned char>& ordered_symbols,
+               size_t message_len) {
 
     std::cout << "### Guardando código ###\n";
 
+    std::bitset<MAX_CODE_SIZE> ulong_bitset = 0xFFFFFFFFFFFFFFFF;
     unsigned char buffer = 0;
     unsigned char buffer_size = 0;
-    char c;
+    unsigned char c;
+
     while (!fin.eof()) {
-        fin >> std::noskipws >> c;
+        fin.read(reinterpret_cast<char*>(&c), sizeof(c));
         auto code = canonical_codes[c].code;
         auto bits = canonical_codes[c].length;
 
         while (bits) {
-            buffer |= (code.to_ulong() << buffer_size);
+            buffer |= (code & ulong_bitset).to_ulong() << buffer_size;
             unsigned char inserted_bits =
                 bits > 8 - buffer_size ? 8 - buffer_size : bits;
             buffer_size += inserted_bits;
@@ -264,33 +258,35 @@ void save_code(std::ofstream& fout, std::ifstream& fin,
     }
 }
 
-void decode_file(std::string& file_name_input, std::string& file_name_output) {
-    std::ifstream fin(file_name_input,
+void decode_file(std::string& file_name_encoded,
+                 std::string& file_name_output) {
+
+    std::ifstream fin(file_name_encoded,
                       std::ifstream::in | std::ifstream::binary);
 
     std::cout << "### Empezando a decodificar ###\n";
     /* Primero extraemos la longitud del mensaje, esta se encuentra en los
      * primeros 4 bytes. */
-    size_t message_len;
+    /* Igualar a cero es importane. */
+    size_t message_len = 0;
     fin.read(reinterpret_cast<char*>(&message_len), 4);
     std::cout << "### Longitud del mensaje: " << message_len << "###\n";
 
     /* Ahora reconstruiremos el mapa de longitudes de código para cada
      * carácter
      */
-    std::unordered_map<char, unsigned char> code_lengths;
-    for (size_t c = 0; c <= 255; ++c) {
+    std::array<unsigned char, CHAR_NUM> code_lengths{0};
+    for (size_t c = 0; c < CHAR_NUM; ++c) {
         unsigned char bit_length = 0;
         fin.read(reinterpret_cast<char*>(&bit_length), 1);
-        if (bit_length)
-            code_lengths[char(c)] = bit_length;
+        code_lengths[(unsigned char)c] = bit_length;
     }
 
     std::cout << "### Generando códigos canónicos para decodificación ###\n";
 
     /* Ahora tenemos que reconstruir los códigos canónicos. */
-    std::vector<char> ordered_symbols;
-    std::unordered_map<char, Code> code_map =
+    std::vector<unsigned char> ordered_symbols;
+    std::array<Code, CHAR_NUM> code_map =
         get_canonical_codes(code_lengths, ordered_symbols);
 
     std::cout << "### Recuperando arbol de Huffman ###\n";
@@ -322,20 +318,26 @@ void decode_file(std::string& file_name_input, std::string& file_name_output) {
         else
             current = current->left;
 
+        if (!current) {
+            std::cout << "oops\n";
+        }
+        current->is_leaf();
         if (current->is_leaf()) {
             ++decoded_symbols;
             fout << current->symbol;
             current = root;
+            std::cout << current->symbol;
         }
     }
 
     fout.close();
 }
 
-void traverse_huffman_tree(
-    Node* root, std::unordered_map<char, unsigned char>& code_map,
-    size_t& total_bits, std::unordered_map<unsigned char, size_t>& length_map,
-    size_t& max_length) {
+void traverse_huffman_tree(Node* root,
+                           std::array<unsigned char, CHAR_NUM>& code_length_map,
+                           size_t& total_bits,
+                           std::array<size_t, CHAR_NUM>& length_frequency_map,
+                           size_t& max_length) {
 
     if (!root)
         return;
@@ -353,8 +355,11 @@ void traverse_huffman_tree(
 
         if (node->is_leaf()) {
             total_bits += node->freq * bit;
-            code_map.insert({node->symbol, bit});
-            ++length_map[bit];
+            code_length_map[node->symbol] = bit;
+            ++length_frequency_map[bit];
+            max_length = length_frequency_map[bit] > max_length
+                             ? length_frequency_map[bit]
+                             : max_length;
         }
         if (node->right) {
             stack->push(std::pair(node->right, bit + 1));
@@ -377,7 +382,7 @@ std::string decode(std::string& coded_message, Node& tree) {
 
     /* Decodificamos */
     Node* v = &tree;
-    for (char c : coded_message) {
+    for (unsigned char c : coded_message) {
         /* Si es 0, vamos al hijo izquierdo, de lo contrario (es 1), vamos a
          * la derecha */
         if (c == '0') {
@@ -398,28 +403,23 @@ std::string decode(std::string& coded_message, Node& tree) {
     return decoded_message;
 }
 
-std::unordered_map<char, size_t>
+std::unordered_map<unsigned char, size_t>
 calculate_frequencies_from_file(std::string& file_name, size_t& message_len) {
-    std::unordered_map<char, size_t> frequencies;
-    std::fstream fin(file_name, std::fstream::in);
-    char c;
-    while (fin >> std::noskipws >> c) {
+    std::unordered_map<unsigned char, size_t> frequencies;
+    std::fstream fin(file_name, std::fstream::in | std::fstream::binary);
+    unsigned char c;
+    while (!fin.eof()) {
+        fin.read(reinterpret_cast<char*>(&c), sizeof(c));
         frequencies[c]++;
         ++message_len;
     }
     return frequencies;
 }
 
-std::unordered_map<char, size_t> calculate_frequencies(std::string& message) {
-    std::unordered_map<char, size_t> frequencies;
-    for (char c : message) {
-        frequencies[c]++;
-    }
-    return frequencies;
-}
 
 /* TODO: Manejar edge case de un carácter */
-Node* generate_huffman_tree(std::unordered_map<char, size_t>& frequencies) {
+Node* generate_huffman_tree(
+    std::unordered_map<unsigned char, size_t>& frequencies) {
 
     std::priority_queue<Node*, std::vector<Node*>, CompareHuffmanNodes>
         prio_queue;
@@ -451,20 +451,22 @@ Node* generate_huffman_tree(std::unordered_map<char, size_t>& frequencies) {
     return root;
 }
 
-Node* generate_huffman_tree(std::unordered_map<char, Code>& code_map) {
+Node* generate_huffman_tree(std::array<Code, CHAR_NUM>& code_map) {
 
     Node* root = new Node(0, 0, nullptr, nullptr, nullptr);
-    for (auto entry : code_map) {
-        auto symbol = entry.first;
-        auto code = entry.second;
-        size_t total_bits = 0;
-        _generate_huffman_tree(symbol, code, root, code.code[0], total_bits);
+    for (size_t symbol = 0; symbol < CHAR_NUM; ++symbol) {
+        if (code_map[(unsigned char)symbol].length) {
+            auto code = code_map[(unsigned char)symbol];
+            size_t total_bits = 0;
+            _generate_huffman_tree(symbol, code, root, code.code[0],
+                                   total_bits);
+        }
     }
     return root;
 }
 
-void _generate_huffman_tree(char symbol, Code& code, Node* node, size_t bit,
-                            size_t& total_bits) {
+void _generate_huffman_tree(unsigned char symbol, Code& code, Node* node,
+                            size_t bit, size_t& total_bits) {
 
     Node* child;
     if (bit) {
@@ -486,7 +488,7 @@ void _generate_huffman_tree(char symbol, Code& code, Node* node, size_t bit,
     }
 }
 
-void increment(std::bitset<255>& bitset) {
+void increment(std::bitset<MAX_CODE_SIZE>& bitset) {
     for (size_t i = 0; i < bitset.size(); ++i) {
         // std::cout << bitset[n_bits - 1 - i] << '\n';
         bitset[i] = bitset[i] ^ 1;
